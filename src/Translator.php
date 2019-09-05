187530b42edb7f4a96c5145eb8c30b270bb6e18a
Jan Habbo Brüning <jan.habbo.bruening@gmail.com>
2019-09-05 11:47:02 +0200

    added base functionality

--- src/Translator.php
+++ src/Translator.php
@@ -8,13 +8,15 @@ namespace Frootbox\Translation;
 class Translator {
 
     protected $data;
+    protected $scope;
+
 
     /**
      *
      */
-    public function get ( $segment ) {
-
-        return $this->data['global'][$segment];
+    public function getData ( ): array
+    {
+        return $this->data;
     }
 
 
@@ -23,8 +25,70 @@ class Translator {
      */
     public function addResource ( $path, $scope = 'global' ): Translator {
 
-        $this->data[$scope] = require $path;
+        $data = require $path;
+
+        foreach ($data as $key => $value) {
+            $this->data[$scope . '.' . $key] = $value;
+        }
 
         return $this;
+    }
+
+
+    /**
+     *
+     */
+    public function setData ( array $data ): Translator
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+
+
+    /**
+     *
+     */
+    public function setLanguage ( $language ): Translator
+    {
+        return $this;
+    }
+
+
+    /**
+     * Set translators current scope
+     */
+    public function setScope ( $scope ): Translator
+    {
+        $this->scope = $scope;
+
+        return $this;
+    }
+
+
+    /**
+     *
+     */
+    public function translate ( $key ): ?string
+    {
+        if (array_key_exists($key, $this->data)) {
+            return $this->data[$key];
+        }
+
+        $segments = explode('.', $this->scope);
+        $loops = count($segments);
+
+        for ($i = 0; $i < $loops; ++$i) {
+
+            $nkey = implode('.', $segments) . '.' . $key;
+
+            if (array_key_exists($nkey, $this->data)) {
+                return $this->data[$nkey];
+            }
+
+            array_pop($segments);
+        }
+
+        return null;
     }
 }
